b341a614103a52cc01ad76df9823f22b33c5a1b9
Aleksandr Suchovarov <aleksandr.suchovarov@gmail.com>
2018-11-30 14:26:37 +0200

    Test unosquare/raspberryio

--- GpioTest/Program.cs
+++ GpioTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
+using Unosquare.RaspberryIO.Peripherals;
 
 namespace GpioTest
 {
@@ -10,26 +12,39 @@ namespace GpioTest
 		{
 			try
 			{
-				var dht = new DHT(Pi.Gpio.Pin07, DHTSensorTypes.DHT22);
-				while (true)
+				using (TemperatureSensorAM2302 sensor = new TemperatureSensorAM2302(Pi.Gpio[P1.Gpio17]))
 				{
-					try
-					{
-						var d = dht.ReadData();
-						Console.WriteLine(DateTime.UtcNow);
-						Console.WriteLine(" temp: " + d.TempCelcius);
-						Console.WriteLine(" hum: " + d.Humidity);
-					}
-					catch (DHTException)
-					{
-					}
-					Thread.Sleep(5000);
+					sensor.OnDataAvailable += Sensor_OnDataAvailable;
+					sensor.Start();
 				}
+
+				Console.ReadLine();
+				//var dht = new DHT(Pi.Gpio.Pin07, DHTSensorTypes.DHT22);
+				//while (true)
+				//{
+				//	try
+				//	{
+				//		var d = dht.ReadData();
+				//		Console.WriteLine(DateTime.UtcNow);
+				//		Console.WriteLine(" temp: " + d.TempCelcius);
+				//		Console.WriteLine(" hum: " + d.Humidity);
+				//	}
+				//	catch (DHTException)
+				//	{
+				//	}
+				//	Thread.Sleep(5000);
+				//}
 			}
 			catch (Exception e)
 			{
 				Console.WriteLine(e.Message + " - " + e.StackTrace);
+				Console.ReadLine();
 			}
+		}
+
+		private static void Sensor_OnDataAvailable(object sender, TemperatureSensorAM2302.AM2302DataReadEventArgs e)
+		{
+			Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] TemperatureCelsius: {e.TemperatureCelsius}, HumidityPercentage: {e.HumidityPercentage}");
 		}
 	}
 }
